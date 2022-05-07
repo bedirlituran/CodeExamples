4241806d6231937edf050f3b93cf54122e89b504
Samir Karimov <trinusd@gmail.com>
2022-05-08 00:39:08 +0400

    Select Badge code comments added

--- SelectBadge/assets/js/app.js
+++ SelectBadge/assets/js/app.js
@@ -1,12 +1,14 @@
-const movies = ["The Protector", "Merlin", "The Letter For The King", "The Order"]
-const badgeContainer = document.querySelector('.badges')
-const badges = document.querySelector('.badges').children
+const movies = ["The Protector", "Merlin", "The Letter For The King", "The Order"]; // data source
+const badgeContainer = document.querySelector('.badges'); // main container 
+const badges = document.querySelector('.badges').children // dynamic elements
 
-
+// Generate UI using movies data source
 movies.forEach((movie, index) => {
     badgeContainer.innerHTML += `<span class="badge bg-primary p-2 m-2" role="button" onclick="selectMovie(this)">${movie}</span>`
 })
 
+
+// select method for every elements
 const selectMovie = (elem) => {
     for (let badge of badges) {
         badge.className = "badge bg-primary p-2 m-2"
