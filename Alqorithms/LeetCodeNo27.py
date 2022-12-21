c7a97745a31a62434f999570873ea2eb36d17b81
Samir Kerimov <samir@pragmatech.az>
2022-12-21 15:14:47 +0400

    LeetCode No.27 Remove Element Algorithm Python Solution

--- Alqorithms/LeetCodeNo27.py
+++ Alqorithms/LeetCodeNo27.py
@@ -1,6 +1,6 @@
 #  LeetCode No.27 Remove Element Algorithm
-
-def removeElement(nums, val):
+#  Version 1.0
+def removeElementWidthPop(nums, val):
     i = 0
     # Dövr yaratmaq üçün i dəyişənini 0-dan başlatdım
     # i<len(nums) şərtini ona görə təyin etdim ki val dəyəri birdən çox ola bilər. Hər uyğun val dəyəri silindikdən sonra dövr normal davam edə bilsin
@@ -13,5 +13,13 @@ def removeElement(nums, val):
             i += 1
     return len(nums)
     
+# Version 2.0
+
+def  removeElementWithAppend(nums,val):
+    requiredList = []
+    for i in range(len(nums)):
+        if nums[i] != val:
+            requiredList.append(nums[i])
     
-   
+    return requiredList 
+print(removeElementWithAppend([3,2,2,3],3))
