e798298df84d9af785b7e200b5ed0d254cf9befb
Samir Kerimov <samir@pragmatech.az>
2023-01-06 09:33:59 +0400

    LeetCode No 26 :  Remove Duplicates from Sorted Array solution with Python

--- Alqorithms/LeetCodeNo26.py
+++ Alqorithms/LeetCodeNo26.py
@@ -1,17 +1,16 @@
 # 26. Remove Duplicates from Sorted Array
 
-class Solution:
-    def removeDuplicates(nums):
-        # Check if the list is empty
-        if len(nums)==0:
-            return 0
-        # Define i as the index of the first element
-        i=0
-        for j in range(1,len(nums)):
-            # Check dublicated value
-            if nums[j]!=nums[i]:
-                # increase i
-                i+=1
-                # Assign value in list
-                nums[i]=nums[j]
-        return i+1
+def removeDuplicates(nums):
+    # Check if the list is empty
+    if len(nums) == 0:
+        return 0
+    # Define i as the index of the first element
+    i = 0
+    for j in range(1, len(nums)):
+        # Check dublicated value
+        if nums[j] != nums[i]:
+            # increase i
+            i += 1
+            # Assign value in list
+            nums[i] = nums[j]
+    return i+1
